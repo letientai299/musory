0f6fc27db7e4ddc4499336577dc0df3fd72589ef
Tai <letientai299@gmail.com>
2022-06-10 20:51:30 +0800

    Implement alternative beats for Metronome

--- src/pages/Metronome.tsx
+++ src/pages/Metronome.tsx
@@ -4,6 +4,47 @@ import { DebounceInput } from 'react-debounce-input'
 import { GiPauseButton, GiPlayButton } from 'react-icons/gi'
 import { Loop, Synth, Transport } from 'tone'
 
+class timeSignature {
+  str: string
+  beats: number // how many beat per measure
+  note: number // what kind of note per beat
+  constructor(sign: string) {
+    this.str = sign
+    const ss = sign.split('/')
+    if (ss.length != 2) {
+      this.beats = 0
+      this.note = 0
+      return
+    }
+    this.beats = Number.parseInt(ss[0])
+    this.note = Number.parseInt(ss[1])
+  }
+}
+
+const timeSignatures: timeSignature[] = ['2/2', '2/4', '3/4', '4/4'].map(
+  (s) => new timeSignature(s)
+)
+
+const TimeSignature = (p: {
+  active: boolean
+  sign: timeSignature
+  onClick: (str: string) => void
+}) => {
+  let cls =
+    'flex border m-6 p-6 flex-col items-center text-center text-3xl font-bold ' +
+    'border-4 hover:border-cyan-500'
+  if (p.active) {
+    cls += ' border-cyan-700'
+  }
+
+  return (
+    <button className={cls} onClick={() => p.onClick(p.sign.str)}>
+      <p>{p.sign.beats}</p>
+      <p>{p.sign.note}</p>
+    </button>
+  )
+}
+
 const defaultBMP = 100
 const minBMP = 10
 const maxBMP = 200
@@ -25,7 +66,7 @@ const safeBmp = (pre: number, n: number) => {
 
 let toneLoop: Loop
 
-function togglePlaying(playing: boolean, bpm: number) {
+function togglePlaying(playing: boolean, bpm: number, sign: string) {
   if (toneLoop) {
     toneLoop.stop(0)
   }
@@ -34,9 +75,17 @@ function togglePlaying(playing: boolean, bpm: number) {
     return
   }
 
+  const timeSign = new timeSignature(sign)
+
   const synth = new Synth().toDestination()
+  let count = 0
   toneLoop = new Loop((time) => {
-    synth.triggerAttackRelease('B2', 0.01, time)
+    if (sign != '' && count % timeSign.beats == 0) {
+      synth.triggerAttackRelease('B2', 0.01, time)
+    } else {
+      synth.triggerAttackRelease('G2', 0.01, time)
+    }
+    count++
   }).start(0)
   Transport.bpm.rampTo(bpm)
   Transport.start()
@@ -45,6 +94,7 @@ function togglePlaying(playing: boolean, bpm: number) {
 const Metronome = () => {
   const [bmp, setBmp] = useState(defaultBMP)
   const [playing, setPlaying] = useState(false)
+  const [signature, setSignature] = useState('')
 
   useHotkeys('up', () => setBmp((pre) => safeBmp(pre, pre + 1)))
   useHotkeys('k', () => setBmp((pre) => safeBmp(pre, pre + 1)))
@@ -54,7 +104,7 @@ const Metronome = () => {
   useHotkeys('Enter', () => setPlaying((pre) => !pre))
   useHotkeys('Space', () => setPlaying((pre) => !pre))
 
-  togglePlaying(playing, bmp)
+  togglePlaying(playing, bmp, signature)
 
   return (
     <div className="m-auto flex flex-col center items-center">
@@ -77,6 +127,20 @@ const Metronome = () => {
       >
         {playing ? <GiPauseButton /> : <GiPlayButton />}
       </button>
+
+      <div className="flex flex-row">
+        {timeSignatures.map((sign) => {
+          const active = signature != '' && signature == sign.str
+          return (
+            <TimeSignature
+              active={active}
+              key={sign.str}
+              sign={sign}
+              onClick={setSignature}
+            />
+          )
+        })}
+      </div>
 
       <article className="prose">
         <ul>
